cc627393dd7126c6155adb94f720d2ef26690556
D'micheal.McElroy <dm237826@gmail.com>
2018-10-18 12:36:44 -0700

    first commit

--- java/investment.js
+++ java/investment.js
@@ -1,13 +1,21 @@
-var additionalInvestment = document.getElementById('').value;
-var InitialInvestment;
-var InvestmentRate;
-var Intrest = 0;
-var TotalInvestment = 0;
-var FinalValue = 0;
-var inner; 
-var outter;
-
-function investment(){
-    var outter = document.getElementById("outter");
-    var inner = document.getElementById("inner")
+function invest() {
+    event.preventDefault();
+    var output = document.getElementById("total_intrest");
+    var output2 = document.getElementById("account_total");
+    var intial_investment = parseInt(document.getElementById("intrest_rate").value);
+    var intrest_rate = parseInt(document.getElementById("time_invested").value);
+    var time_invested = parseInt(document.getElementById("time_invested").value);
+    var contributions = parseInt(document.getElementById("contributions").value);
+    var investment_term = "";
+    var selectReturn = document.querySelector('input[name="term"]:checked').value;
+    if (selectReturn ==1) {
+        investment_term = time_invested;
+    } else if (selectReturn ==2){
+        investment_term = time_invested / 12;
+    }
+    var total_contributions = investment_term * contributions;
+    var total_intrest = (intrest_rate / 100) * (total_contributions + intial_investment);
+    var account_total = total_intrest + total_contributions + intial_investment;
+    output.innerHTML = total_intrest;
+    output2.innerHTML = account_total;
 }
